4f06875de9375775a207c00f0f7ec1e049371318
sowon <fkdla1024@naver.com>
2025-08-05 14:39:16 +0900

    init: spring-postgis 프로젝트 초기 커밋

---
+++ src/main/java/com/example/postgis_demo/PostgisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postgis_demo/PostgisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgis_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgisDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
